e67e201f18e87b88eb20eab76d1aed777986b5c3
ku-mu* <kuhmu>
2013-08-17 00:36:45 +0900

    Decode strings in db

--- lib/Intern/Diary/Model/Entry.pm
+++ lib/Intern/Diary/Model/Entry.pm
@@ -5,6 +5,7 @@ use warnings;
 use utf8;
 
 use JSON::Types qw();
+use Encode qw(decode_utf8);
 
 use Class::Accessor::Lite (
     ro => [qw(
@@ -36,8 +37,8 @@ sub TO_JSON {
     my ($self) = @_;
     return {
         entry_id    => JSON::Types::number $self->entry_id,
-        title       => $self->title,
-        body        => $self->body,
+        title       => decode_utf8($self->title),
+        body        => decode_utf8($self->body),
         diary_id    => JSON::Types::string $self->diary_id,
         created     => JSON::Types::string $self->created,
         updated     => JSON::Types::string $self->updated,
